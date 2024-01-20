17e77a4e7eb6dfb6ade9c4ec04f6c8919da9f0d9
Achraftraf <achrafzarouki20@gmail.com>
2024-01-20 11:38:03 +0100

    this is the component traning

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { AppComponent } from './app.component';
 import { RegisterComponent } from './components/register/register.component';
 import { LoginComponent } from './components/login/login.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { TrainingFormComponent } from './components/training-form/training-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     RegisterComponent,
     LoginComponent,
-    DashboardComponent
+    DashboardComponent,
+    TrainingFormComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/training-form/training-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-training-form',
+  templateUrl: './training-form.component.html',
+  styleUrls: ['./training-form.component.scss']
+})
+export class TrainingFormComponent {
+
+}
